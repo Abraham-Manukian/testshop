70dcbc80cc3f549d9ac02dd2ffdf3e28c4229c0f
Abraham <agm041214@gmail.com>
2022-11-04 00:25:36 +0300

    fragment_select_category

--- app/src/main/java/com/example/testshop/MainActivity.kt
+++ app/src/main/java/com/example/testshop/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.testshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testshop.screen_main.select_category
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.placeHolder, select_category.newInstance()).commit()
     }
 }
