70a5220fce2f3c8e1759673ff7878b71caaaacd7
Abraham <agm041214@gmail.com>
2022-11-04 22:21:58 +0300

    2 day

--- app/src/main/java/com/example/testshop/screen_main/select_category.kt
+++ app/src/main/java/com/example/testshop/screen_main/select_category.kt
@@ -1,21 +1,31 @@
 package com.example.testshop.screen_main
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.testshop.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.testshop.databinding.FragmentSelectCategoryBinding
 
+
 class select_category : Fragment() {
+
+
+
     private lateinit var binding: FragmentSelectCategoryBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSelectCategoryBinding.inflate(inflater, container, false)
+
+        binding.button1.setOnClickListener{
+            binding.button1.background.setTint(Color.GREEN)
+        }
         return binding.root
     }
 
@@ -23,4 +33,6 @@ class select_category : Fragment() {
         @JvmStatic
         fun newInstance() = select_category()
     }
+
+
 }
