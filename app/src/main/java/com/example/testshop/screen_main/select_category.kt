621f99262fca69672bd5abfe2115936dec24945f
Abraham <agm041214@gmail.com>
2022-11-08 23:09:38 +0300

    fun for button

--- app/src/main/java/com/example/testshop/screen_main/select_category.kt
+++ app/src/main/java/com/example/testshop/screen_main/select_category.kt
@@ -1,20 +1,23 @@
 package com.example.testshop.screen_main
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.testshop.R
 import com.example.testshop.databinding.FragmentSelectCategoryBinding
+import java.time.temporal.ValueRange
 
 
 class select_category : Fragment() {
 
 
-
+    var a = 10
     private lateinit var binding: FragmentSelectCategoryBinding
+    var flag = true
 
 
     override fun onCreateView(
@@ -23,10 +26,43 @@ class select_category : Fragment() {
     ): View? {
         binding = FragmentSelectCategoryBinding.inflate(inflater, container, false)
 
-        binding.button1.setOnClickListener{
-            binding.button1.background.setTint(Color.GREEN)
+
+        binding.button2.setOnClickListener {
+            /*if (flag) {
+                binding.button2.setImageResource(R.drawable.ic_button_select_category)
+                flag = false
+            } else {
+                binding.button2.setImageResource(R.drawable.ic_button_select_category_on)
+                flag = true
+
+            }*/
+            buttonclick(1)
         }
+        binding.button1.setOnClickListener{buttonclick(0)}
+        binding.button3.setOnClickListener{buttonclick(2)}
+        binding.button4.setOnClickListener{buttonclick(3)}
         return binding.root
+    }
+    var buttons = arrayOf(
+        binding.button1,
+        binding.button2,
+        binding.button3,
+        binding.button4)
+
+
+    private fun buttonclick(x: Int) {
+        if (flag){
+            buttons[x].setImageResource(R.drawable.ic_button_select_category_on)
+            buttons[x].setBackgroundResource(R.drawable.button_select_category_off)
+            flag = false
+        }
+        else{
+            buttons[x].setImageResource(R.drawable.ic_button_select_category)
+            buttons[x].setBackgroundResource(R.drawable.button_select_category_on)
+            flag = true
+        }
+
+
     }
 
     companion object {
@@ -36,3 +72,6 @@ class select_category : Fragment() {
 
 
 }
+
+
+
