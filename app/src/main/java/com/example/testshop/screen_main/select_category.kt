8ccf2efcdef41d7ee7cf5c9e7a0b51d7a7998127
Abraham <agm041214@gmail.com>
2022-11-10 22:02:37 +0300

    icon

--- app/src/main/java/com/example/testshop/screen_main/select_category.kt
+++ app/src/main/java/com/example/testshop/screen_main/select_category.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.testshop.R
 import com.example.testshop.databinding.FragmentSelectCategoryBinding
-import java.time.temporal.ValueRange
 
 
 class select_category : Fragment() {
@@ -28,27 +25,26 @@ class select_category : Fragment() {
 
         binding.button1.setOnClickListener {
             buttonclick()
-                binding.button1.setImageResource(R.drawable.ic_button_select_category)
-                binding.button1.setBackgroundResource(R.drawable.button_select_category_on)
+                binding.button1.setImageResource(R.drawable.ic_button_select_category_phone_on)
+                binding.button1.setBackgroundResource(R.drawable.button_select_category)
         }
 
         binding.button2.setOnClickListener {
                 buttonclick()
-                binding.button2.setImageResource(R.drawable.ic_button_select_category)
-                binding.button2.setBackgroundResource(R.drawable.button_select_category_on)
+                binding.button2.setImageResource(R.drawable.ic_button_select_category_comp_on)
+                binding.button2.setBackgroundResource(R.drawable.button_select_category)
         }
 
         binding.button3.setOnClickListener {
                 buttonclick()
-                binding.button3.setImageResource(R.drawable.ic_button_select_category)
-                binding.button3.setBackgroundResource(R.drawable.button_select_category_on)
+                binding.button3.setImageResource(R.drawable.ic_button_select_category_health_on)
+                binding.button3.setBackgroundResource(R.drawable.button_select_category)
         }
 
         binding.button4.setOnClickListener {
             buttonclick()
-                binding.button4.setImageResource(R.drawable.ic_button_select_category)
-                binding.button4.setBackgroundResource(R.drawable.button_select_category_on)
-
+                binding.button4.setImageResource(R.drawable.ic_button_select_category_book_on)
+                binding.button4.setBackgroundResource(R.drawable.button_select_category)
         }
         return binding.root
     }
@@ -60,16 +56,16 @@ class select_category : Fragment() {
 
 
     private fun buttonclick (){
-        binding.button1.setImageResource(R.drawable.ic_button_select_category_on)
+        binding.button1.setImageResource(R.drawable.ic_button_select_category_phone_off)
         binding.button1.setBackgroundResource(R.drawable.button_select_category_off)
 
-        binding.button2.setImageResource(R.drawable.ic_button_select_category_on)
+        binding.button2.setImageResource(R.drawable.ic_button_select_category_comp_off)
         binding.button2.setBackgroundResource(R.drawable.button_select_category_off)
 
-        binding.button3.setImageResource(R.drawable.ic_button_select_category_on)
+        binding.button3.setImageResource(R.drawable.ic_button_select_category_health_off)
         binding.button3.setBackgroundResource(R.drawable.button_select_category_off)
 
-        binding.button4.setImageResource(R.drawable.ic_button_select_category_on)
+        binding.button4.setImageResource(R.drawable.ic_button_select_category_book_off)
         binding.button4.setBackgroundResource(R.drawable.button_select_category_off)
 
 
